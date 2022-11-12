ad7acaded65f1e65f1908e690c6e2a1fa95802c3
prashant <prashantmiracle@gmail.com>
2022-11-12 17:06:34 +0530

    2

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from "react";
+import "./header.css";
 
 export function Header(props) {
-    
-
-    return (
-        <>
-            this is header
-        </>
-    )
+  return (
+    <div className="header">
+      <div className="logo"> Prashant Kumar </div>
+      <div className="menu">
+        <div className="web-menu">
+          <web />
+        </div>
+        <div className="mobile-menu">
+          <mobile />
+        </div>
+      </div>
+    </div>
+  );
 }
-export default Header
+export default Header;
---
+++ src/components/header/web/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function web(props) {
+  return <>this is web</>;
+}
