b70005f827dae5f7f88842bf95d5629257531990
prashant <prashantmiracle@gmail.com>
2022-11-13 23:03:14 +0530

    144

--- src/components/body/projects/project-card.js
+++ src/components/body/projects/project-card.js
@@ -4,7 +4,9 @@ import "./project-card.css";
 export function ProjectCard(project) {
   return (
     <div className="project-card">
-      <div className="project-info"></div>
+      <div className="project-info">
+        <label>{project.title}</label>
+      </div>
 
       <img src={project.image} className="project-photo" alt="" />
     </div>
