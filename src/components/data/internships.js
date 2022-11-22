b7587e8050285ad4ea6f53bcd65208f35dc9bf79
prashant <prashantmiracle@gmail.com>
2022-11-22 12:32:03 +0530

    4

--- src/components/data/internships.js
+++ src/components/data/internships.js
@@ -6,6 +6,10 @@ export const InternshipsData = [
         name: "Coal India-May-June 2007",
         icon: require("../../assets/images/coal.png"),
       },
+      {
+        name: "IOCL, Begusarai -Jan-Feb 2008",
+        icon: require("../../assets/images/IOCL.png"),
+      },
     ],
   },
   {
@@ -14,6 +18,14 @@ export const InternshipsData = [
       {
         name: "RSWM LTD-Dec- Jan 2011",
         icon: require("../../assets/images/RSWM.png"),
+      },
+      {
+        name: "JM Fabrics Dhaka-Feb- May 2012",
+        icon: require("../../assets/images/JM.png"),
+      },
+      {
+        name: "Soma Textile & Industries-May-Aug 2011",
+        icon: require("../../assets/images/SOMA.png"),
       },
     ],
   },
