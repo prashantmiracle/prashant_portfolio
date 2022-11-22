5b2668bd82b9a330445319890ab08e997cb4e827
prashant <prashantmiracle@gmail.com>
2022-11-22 13:26:57 +0530

    6

--- src/components/data/activities.js
+++ src/components/data/activities.js
@@ -1,31 +1,19 @@
 export const ActivitiesData = [
   {
-    type: "IT",
+    type: "Paper Presentation",
     list: [
       {
         name: "Coal India-May-June 2007",
         icon: require("../../assets/images/coal.png"),
       },
-      {
-        name: "IOCL, Begusarai -Jan-Feb 2008",
-        icon: require("../../assets/images/IOCL.png"),
-      },
     ],
   },
   {
-    type: "Fashion ",
+    type: "Workshop ",
     list: [
       {
         name: "RSWM LTD-Dec- Jan 2011",
         icon: require("../../assets/images/RSWM.png"),
-      },
-      {
-        name: "JM Fabrics Dhaka-Feb- May 2012",
-        icon: require("../../assets/images/JM.png"),
-      },
-      {
-        name: "Soma Textile & Industries-May-Aug 2011",
-        icon: require("../../assets/images/SOMA.png"),
       },
     ],
   },
