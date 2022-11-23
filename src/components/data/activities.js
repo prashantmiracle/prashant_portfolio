fcf396cb3c6b7e01fd0bd9ff447bb3f4b5700aa7
prashant <prashantmiracle@gmail.com>
2022-11-23 09:05:49 +0530

    1

--- src/components/data/activities.js
+++ src/components/data/activities.js
@@ -3,14 +3,26 @@ export const ActivitiesData = [
     type: "Paper Presentation",
     list: [
       {
+        name: "Sankalp 07 ",
+        icon: require("../../assets/images/nist.png"),
+      },
+      {
         name: "Advaita 07",
         icon: require("../../assets/images/giet.png"),
+      },
+      {
+        name: "Samavesh 08 ",
+        icon: require("../../assets/images/uce.png"),
       },
     ],
   },
   {
     type: "Workshop ",
     list: [
+      {
+        name: "Ghani Zaman 2008",
+        icon: require("../../assets/images/ghani.png"),
+      },
       {
         name: "Anveshan 2009",
         icon: require("../../assets/images/mirc.png"),
