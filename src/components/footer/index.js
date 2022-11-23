2658d2faec70f1621e8bd1fd999e0541848eaa7e
prashant <prashantmiracle@gmail.com>
2022-11-23 08:11:14 +0530

    1

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./footer.css";
 export default function Footer() {
-  return <div className="footer">Made by Prashant Kumar</div>;
+  return (
+    <div className="footer">
+      All trademarks cited herein are the property of their respective owners.
+    </div>
+  );
 }
