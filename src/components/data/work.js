1700abf2210ab75bc6383df86c6275883100b7be
prashant <prashantmiracle@gmail.com>
2022-11-25 02:52:25 +0530

    2

--- src/components/data/work.js
+++ src/components/data/work.js
@@ -22,7 +22,7 @@ export const WorkData = [
     dateJoining: "August 2015",
     dateEnd: "October 2016",
     CompanyLogo: require("../../assets/images/paytm.png"),
-    work: "My role was to reduce the return rate of products, which was a major issue at Paytm. I worked with Category Team to help them understand their quality issues. I also worked with Operations to understand how we could make the process more efficient.",
+    work: "My role was to reduce the return rate of products, which was a major issue at Paytm. I worked with Category Team to help them understand their quality issues. I also worked with Operations to understand how we could make the process more efficient.In 2015-16, I achieved rising star status and improved processes through kaizen implementation.",
   },
   {
     company: " ESSELL Sourcing Co, New Delhi",
@@ -30,7 +30,7 @@ export const WorkData = [
     dateJoining: "December 2013",
     dateEnd: "March 2015",
     CompanyLogo: require("../../assets/images/ESSELL.png"),
-    work: "When I first started at the company, my focus was on the merchandising side. I had a lot of fun handling clients' accounts, but once I had those up and running, I took on a bigger role in marketing and business development. In 2015-16, I achieved rising star status and improved processes through kaizen implementation.",
+    work: "When I first started at the company, my focus was on the merchandising side. I had a lot of fun handling clients' accounts, but once I had those up and running, I took on a bigger role in marketing and business development. ",
   },
   {
     company: " CyberQ consulting Pvt Ltd.",
